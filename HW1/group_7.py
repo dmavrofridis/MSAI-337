6b8d951c389ff68bca3bdbd7c1150643bfe873a3
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-01-22 17:15:46 -0600

    Needs Review, added code for Q1-Q4

--- HW1/group_7.py
+++ HW1/group_7.py
@@ -6,4 +6,36 @@ def question_7(text):
 
     #decode the text using the same corpus
 
-def encode_text_as_int(text)
+def encode_text_as_int(text):
+    intvector = []
+
+    splittext = [token.lower() for token in word_tokenize(text)]
+
+    for word in splittext:
+        if word.lower() in word_indices.keys():
+            intvector.append(word_indices[word.lower()])
+        else:
+            intvector.append(-1);
+
+    print(text, "encoded to ", intvector)
+
+    return intvector
+
+def decode_ints_as_text(int_vector):
+    outstring = ""
+
+    for i in int_vector:
+        if i != -1:
+            #poor man's implementation
+            for key, value in word_indices.items():
+                if value == i:
+                    print(i, " ", value, " ", key)
+                    outstring += " "
+                    outstring += key
+                    continue
+        else:
+            outstring += " <unk>"
+
+    print(int_vector, "decoded to ", outstring)
+
+    return outstring
