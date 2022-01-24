8326745fe177d84d5d98cf596cdc03c06effb71a
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-01-23 23:02:57 -0600

    Merge branch 'main' of /Users/aleksandrsimonyan/Documents/GitHub/MSAI-337 with conflicts.

--- HW1/starter.py
+++ HW1/starter.py
@@ -1,9 +1,9 @@
 import nltk
 import re
 from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 from sklearn.model_selection import train_test_split
 import numpy as np
+#nltk.download()
 
 
 class my_corpus:
@@ -49,13 +49,12 @@ def question_1(text):
 
 
 def question_2(text):
-
     for i in range(len(text)):
         print(text[i])
         text[i] = re.sub(r'^([0-9]{4})$', '<date>', text[i])
         text[i] = re.sub(r'([0-9]+\.[0-9]+)', '<decimal>', text[i])
         text[i] = re.sub(r'^([0-9]{2})$', '<day>', text[i])
-        text[i] = re.sub(r'^([0-9]+[^0-9][0-9]+)$', '<other>', text[i])
+        text[i] = re.sub(r'^([0-9]+[^\.0-9][0-9]+)$', '<other>', text[i])
         text[i] = re.sub(r'[0-9]+', '<integer>', text[i])
     return text
 
@@ -65,7 +64,8 @@ def question_3(text):
     validation_data, testing_data = train_test_split(testing_data, test_size=0.5, random_state=25)
     return training_data, testing_data, validation_data
 
-
+#training_data, testing_data, validation_data = question_3(text)
+#QUESTION 4 FAMILY STARTS
 def question_4(text, stopwords):
     maximum_frequency = 3.0
     word_frequencies = {}
@@ -76,17 +76,151 @@ def question_4(text, stopwords):
             else:
                 word_frequencies[word] += 1
 
-    # Cap to the frequency of 3.0
+    # Cap to the frequence of 3.0
     for word in word_frequencies.keys():
         word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
 
-    #print(word_frequencies)
-    print("WORD INDECES -> " + str(word_indices))
+
+def question_4_i(corpus):
+    count = 0
+    for token in set(corpus):
+        count +=1
+    return count
+
+
+
+
+def question_4_ii(training_corpus):
+    count = 0
+    for token in (training_corpus):
+        count +=1
+    return count
+
+
+
+def word_to_unk(training_corpus, frequency = 5):
+    word_frequncy = {}
+
+    for token in training_corpus:
+        if token in word_frequncy:
+            word_frequncy[token] +=1
+        else:
+            word_frequncy[token] = 1
+    new_training_corpus=[]
+    for token in training_corpus:
+        if word_frequncy[token] <=frequency:
+            new_training_corpus.append('<unk>')
+        else:
+            new_training_corpus.append(token)
+
+    return new_training_corpus
+
+def question_iii(new_training_corpus):
+    return sum(([1 for i in new_training_corpus if i =='<unk>']))
+
+
+
+def question_iv(validation_data, training_data):
+    count =0
+    for token in validation_data:
+        if token not in training_data:
+            count +=1
+            print(count)
+    return count
+
+
+def question_v(new_training_data):
+    number = [str(i) for i in range(10)]
+    type_token ={'string':0, 'number':0}
+
+    for token in new_training_data:
+        if token[-1] in number:
+            type_token['number'] +=1
+        else:
+            type_token['string'] +=1
+    return type_token
+
+
+
+
+def question_vi(training_data,stopwords):
+    count = 0
+    for token in training_data:
+        if token in stopwords:
+            count +=1
+    return count
+
+#most encountered
+def custom_metric_1(new_training_data, number_of_top_words = 30):
+    word_frequncy = {}
+
+    for token in new_training_data:
+        if token in word_frequncy:
+            word_frequncy[token] +=1
+        else:
+            word_frequncy[token] = 1
+
+    lis = sorted(word_frequncy, key=word_frequncy.get, reverse=True)[:number_of_top_words]
+    final_dic = {}
+    for name in lis:
+        final_dic[name] = word_frequncy[name]
+    return final_dic
+
+#n_gram_creator
+def custom_metric_2(new_training_data, n=3, number_of_top_words = 30):
+    ngram_list = []
+    for index in range(len(new_training_data)):
+        if index >= n:
+            st = ''
+            for i in reversed(range(n)):
+                st += new_training_data[index -i] + ' '
+            ngram_list.append(st)
+    return custom_metric_1(ngram_list, number_of_top_words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# QUESTION 4 FAMILY ENDS
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
-
     stopwords = set_up()
+
     text = read_file("source_text.txt")
     corpus = my_corpus(None)
     # text = input('Please enter a test sequence to encode and recover: ')
@@ -94,21 +228,40 @@ def main():
     # Question 1
     text = question_1(text)
     # Question 2
-    text = question_2(text)
+    #text = question_2(text)
     # Question 3
-    training_set, testing_set, validation_set = question_3(text)
+    training_data, testing_data, validation_data = question_3(text)
+    training_data_without_stop_words = [token for token in training_data if token not in stopwords]
+    validation_data_data_without_stop_words = [token for token in validation_data if token not in stopwords]
+    testing_data_data_without_stop_words = [token for token in testing_data if token not in stopwords]
+
     # Question 4
-    question_4(text, stopwords)
+    print('training_data_token_count' + " " + str(question_4_i(training_data_without_stop_words)))
+    print('validation_data_token_count' + " " + str(question_4_i( validation_data_data_without_stop_words)))
+    print('test_data_token_count' + " " + str(question_4_i(testing_data_data_without_stop_words)))
+    print('vocabruary_size' + " " + str(question_4_ii( training_data_without_stop_words)))
 
-    print(' ')
-    ints = corpus.encode_as_ints(text)
-    print(' ')
-    print('integer encodeing: ', ints)
-    print(' ')
+    new_training_data = word_to_unk(training_data_without_stop_words)
+    print('number_of_unk' +  "   " + str(question_iii(training_data_without_stop_words)))
+    print('out_of_words' +  "   " + str(question_iv(validation_data_data_without_stop_words, new_training_data)))
+    print('number_of_types' +  "   " + str(question_v(new_training_data)))
+    print('number_of_stopwords' +  "   " + str(question_vi(training_data, stopwords)))
+    print('top_words' +  "   " + str(custom_metric_1(new_training_data, number_of_top_words = 30)))
+    print('ngram_words'+"  "+    str(custom_metric_2(new_training_data, number_of_top_words=120)))
+# QUESTION 4 ends
 
-    text = corpus.encode_as_text(ints)
-    print(' ')
-    print('this is the encoded text: %s' % text)
+    #print(question_4(text, stopwords))
+
+
+    #print(' ')
+    #ints = corpus.encode_as_ints(text)
+    #print(' ')
+    #print('integer encodeing: ', ints)
+    #print(' ')
+
+    #text = corpus.encode_as_text(ints)
+    #print(' ')
+    #print('this is the encoded text: %s' % text)
 
 
 if __name__ == "__main__":
