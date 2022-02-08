2dd779f034970705cf6f41c748b727a51e07f0f7
AleksandrSim <46563315+AleksandrSim@users.noreply.github.com>
2022-02-07 23:42:49 -0600

    Feed Forward in progress

---
+++ HW2/FeedForwardNetwork.py
@@ -0,0 +1,41 @@
+import torch.nn
+from torch import nn
+import time
+
+class FeedForward(nn.Module):
+    def __init__(self, input_size, number_of_classes, embedding_space):
+        super(FeedForward, self).__init__()
+        self.Linear1 = nn.Linear(input_size, embedding_space)
+        self.activation = torch.nn.Sigmoid()
+        self.Linear2 = nn.Linear(embedding_space, number_of_classes)
+        self.softmax = torch.nn.Softmax()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.Linear1 =self.Linear1(-initrange, initrange)
+
+    def forward(self, x):
+        x = self.activation(self.Linear1(x))
+        x = self.softmax(self.Linear2(x))
+
+
+        return x
+
+def train(dataloader, model, optimizer, criterion, epoch = 20):
+    running_loss = 0
+    model.train()
+
+
+    for i, data in enumerate(dataloader):
+        X,y = data
+        optimizer.zero_grad()
+        predictions = model(X)
+        loss = criterion(predictions,y)
+        loss.backward()
+        optimizer.step()
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+    print('Finished Training')
--- HW2/main.py
+++ HW2/main.py
@@ -1,9 +1,9 @@
 import preprocessing
 import dataloader
 import time
-
-
-
+import FeedForwardNetwork
+import torch.optim as optim
+import torch.nn as nn
 
 def main():
     start = time.time()
@@ -14,18 +14,59 @@ def main():
     text = preprocessing.remove_stopwords(text)
     text = preprocessing.to_number(text)
     unique_n = dataloader.unique_words(text)
-    print('number_of_all_words ------> ' + str(dataloader.overall_words(text)))
-    print('number_of_unique_words ---> '+ str(dataloader.unique_words(text)))
+    print('unique_words----->' + str( unique_n))
+
+    mapping = dataloader.create_integers(text)
+    reverse_mapping = {i:k for k,i in mapping.items()}
+    integers_texts = dataloader.words_to_integers(text, mapping)# This is X
+    slised_integers = dataloader.sliding_window(integers_texts, 5)
+    slised_integers = slised_integers[:-1] #you cannot predict the final one
+
+    labels = dataloader.label_generation(integers_texts)
+    print('length_of_input_feature----->' + str( len(slised_integers)))
+    print('length_of_target_variables----->' + str( len(labels )))
+
     one_hot_dic = dataloader.create_one_hot_encoddings(text, unique_n)
-    one_hot_representation = dataloader.map_words_to_vec(text, one_hot_dic)
+    labels_to_vectors = dataloader.integers_to_vectors(labels, reverse_mapping, one_hot_dic)# This is Y
+    dataset =  dataloader.wikiDataset( slised_integers, labels_to_vectors)
+    dataset_with_batch = dataloader.batch_divder(dataset, batch_size=20)
+    net = FeedForwardNetwork.FeedForward(input_size=5, number_of_classes= 27422, embedding_space=100)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.005)
+    FeedForwardNetwork.train( dataset_with_batch, net,optimizer , criterion, 20 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #print('number_of_all_words ------> ' + str(dataloader.overall_words(text)))
+   # print('number_of_unique_words ---> '+ str(dataloader.unique_words(text)))
+   # one_hot_representation = dataloader.map_words_to_vec(text, one_hot_dic)
 
     #vectorized_tensors = [torch.LongTensor(vector) for vector in one_hot_representation]
 
 
-    vectorized_tensors = dataloader.wikiDataset(one_hot_representation)
-    print('shape_of_1_smaple_data_is---------->' + str( vectorized_tensors[0].shape))
-    print('type_of_smaple_data_is---------->' + str(type(vectorized_tensors)))
-    loadedData = dataloader.batch_divder(vectorized_tensors, batch_size=20)
+    #vectorized_tensors = dataloader.wikiDataset(one_hot_representation)
+    #print('shape_of_1_smaple_data_is---------->' + str( vectorized_tensors[0].shape))
+ #   print('type_of_smaple_data_is---------->' + str(type(vectorized_tensors)))
+
+
+    #loadedData = dataloader.batch_divder(vectorized_tensors, batch_size=20)
+    #lis = []
+
+
+
+
 
 
 
