0b23838c63d5e9a3c7e7a236e620b201988e6947
dmavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-03-10 20:22:26 -0600

    Update

--- Final Project/run_clm.py
+++ Final Project/run_clm.py
@@ -555,5 +555,5 @@ if __name__ == "__main__":
     print("Device is -> " + str(device) + " and number of devices available -> " + str(available_gpus))
     '''--output_dir=files/data/output/b --model_type=gpt2 --model_name_or_path=gpt2 
     --train_file=files/data/output/woz.train_b.txt --do_train --validation_file=files/data/output/woz.test_b.txt 
-    --do_eval --save_total_limit 10 --num_train_epochs 10 --device=cuda --n_gpu=1'''
+    --do_eval --save_total_limit 10 --num_train_epochs 10 --per_device_train_batch_size=1 --per_device_eval_batch_size=1'''
     main()
