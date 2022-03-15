7531ce7eafb26fca7f25e5a1d5dff3c87d5468d6
Dimitrios Mavrofridis <91025661+dmavrofridis@users.noreply.github.com>
2022-03-15 00:17:37 -0500

    Delete Final Project directory

--- Final Project/fine_tune_fpt2.sh
+++
@@ -1,1 +0,0 @@
-python run_clm.py --output_dir=files/data/output/b --model_type=gpt2 --model_name_or_path=gpt2 --train_file=files/data/output/woz.train_b.txt --do_train --validation_file=files/data/output/woz.test_b.txt --do_eval --save_total_limit 10 --num_train_epochs 10 --per_device_train_batch_size=2 --per_device_eval_batch_size=2
--- Final Project/global_variables.py
+++
@@ -1,3 +0,0 @@
-input_train_data_path = "files/data/input/train"
-input_data_path = "files/data/input"
-output_data_path = "files/data/output"
